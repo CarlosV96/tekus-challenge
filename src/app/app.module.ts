facbca794e63a6cd31279d1a91f904d8fd4cdc02
Carlos Andres Valencia <anvaloso1@gmail.com>
2022-08-31 22:22:28 -0500

    Layouts screens challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SubscribersComponent } from './subscribers/subscribers.component';
+import { AddSubscriberComponent } from './add-subscriber/add-subscriber.component';
+import { EditSubscriberComponent } from './edit-subscriber/edit-subscriber.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SubscribersComponent,
+    AddSubscriberComponent,
+    EditSubscriberComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/edit-subscriber/edit-subscriber.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'edit-subscriber',
+  templateUrl: './edit-subscriber.component.html',
+  styleUrls: ['./edit-subscriber.component.css']
+})
+export class EditSubscriberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
